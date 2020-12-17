0b1b1a7270582fb462563b74695c36f037b5cccc
yangbashan <15733097205@163.com>
2020-12-17 10:22:24 +0800

    布局组件优化

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,18 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // menu闭合状态 false不闭合 true闭合
+    collapsed: false,
   },
   mutations: {
+    changeCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
   },
   actions: {
+    changeCollapsed({ commit }) {
+      commit('changeCollapsed');
+    },
   },
   modules: {
   },
