f40b8617facae066c3d4444e15ea7dd2614c5f21
yangbashan <15733097205@163.com>
2020-12-20 02:54:51 +0800

    search表单组件

--- src/views/page/productList.vue
+++ src/views/page/productList.vue
@@ -1,3 +1,21 @@
 <template>
-    <div>商品列表</div>
+    <div class="product-list">
+        <search-box @submit="searchSubmit" />
+    </div>
 </template>
+
+<script>
+import SearchBox from '@/components/search.vue';
+
+export default {
+
+  components: {
+    SearchBox,
+  },
+  methods: {
+    searchSubmit(form) {
+      console.log(form);
+    },
+  },
+};
+</script>
