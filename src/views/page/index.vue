797d10c41049d1ad56a008dae562a0804711ee65
yangbashan <15733097205@163.com>
2020-12-19 12:33:09 +0800

    统计&可视化

--- src/views/page/index.vue
+++ src/views/page/index.vue
@@ -1,3 +1,18 @@
 <template>
-    <div>统计</div>
+    <div class="index">
+        <card />
+        <Charts />
+    </div>
 </template>
+
+<script>
+import Card from '@/components/card.vue';
+import Charts from '@/components/charts.vue';
+
+export default {
+  components: {
+    Card,
+    Charts,
+  },
+};
+</script>
